3a70cfe3002e67feca2d2b24eab68de568946ec4
2300033794 <2300033794@kluniversity.in>
2025-09-07 19:43:42 +0530

    updated backend

---
+++ Backend2/src/main/java/com/klef/dev/Backend2Application.java
@@ -0,0 +1,14 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Backend2Application extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend2Application.class, args);
+	}
+
+}
